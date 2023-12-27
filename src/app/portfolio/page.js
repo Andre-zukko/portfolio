f39d47595cc6e44284f9a0585111de41779c4b5f
Andre-zukko <145490579+Andre-zukko@users.noreply.github.com>
2023-12-27 21:49:27 +0100

    Update page.js

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -9,11 +9,11 @@ const Portfolio = () => {
       <h1 className='font-bold text-center text-4xl text-blue-500 '>My work</h1>
 
       <div className='flex flex-col justify-center items-center h-[100vh]'>
-        <div className=' text-9xl text-blue-500'> 
+        <div className=' text-9xl text-blue-500 max-sm:mb-14'> 
           <FaRegImages /> 
         </div>
-        <h2 className='text-slate-500 text-2xl flex items-center justify-center h-20'>This section 
-          <span className='border-b-4 border-blue-500 font-semibold ml-2 mr-2'>is still waiting</span>
+        <h2 className='text-slate-500 text-2xl flex items-center justify-center h-20 max-sm:flex-col max-sm:text-center'>This section 
+          <span className='border-b-4 border-blue-500 font-semibold ml-2 mr-2 max-sm:border-none'>is still waiting</span>
           for your 
           <span className='text-blue-500 font-semibold ml-2 mr-2'>offers</span>
           <span className='text-4xl text-blue-500'> <MdOutlineLocalOffer /> </span>
